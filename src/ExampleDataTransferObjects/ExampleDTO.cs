e3a87738449c7d0d39b321eaad4c7f0bf2fc16ac
SANLAB\ismail.turkmenoglu <ismail.turkmenoglu@sanlab.net>
2022-03-03 14:03:58 +0300

    Socket programming.

---
+++ src/ExampleDataTransferObjects/ExampleDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ExampleDataTransferObjects
+{
+    /// <summary>
+    /// Serialize edebilmek için Serializable attributü ile işaretliyoruz.
+    /// </summary>
+    [Serializable]
+    public class ExampleDTO
+    {
+        public string Status { get; set; }
+        public string Message { get; set; }
+    }
+}
